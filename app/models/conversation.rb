bfc3ebb34a677db77948006be1eaa50147c9a173
trose618 <trose618@gmail.com>
2019-02-10 23:59:27 -0500

    added calendar, need to import lessons

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,6 @@ class Conversation < ApplicationRecord
     has_many :messages
     belongs_to :user_one, class_name: :User
     belongs_to :user_two, class_name: :User
+
+    validates :title, uniqueness: { case_sensitive: false }
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ApplicationRecord
   belongs_to :coach, class_name: :User
   belongs_to :client, class_name: :User
+
+  validates :title, uniqueness: { case_sensitive: false }
 end
