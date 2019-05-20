9a620064bb95a1623e04eb5480474a31b8216401
trose618 <trose618@gmail.com>
2019-05-19 22:42:27 -0400

    testing connecting user to lessons_channel

--- app/channels/lessons_channel.rb
+++ app/channels/lessons_channel.rb
@@ -1,6 +1,11 @@
 class LessonsChannel < ApplicationCable::Channel
     def subscribed
-        stream_from "lessons_channel"
+        stream_for current_user
+        binding.pry
+    end
+
+    def received(data)
+      LessonsChannel.broadcast_to(current_user, alert: "lesson update!")
     end
   
     def unsubscribed
